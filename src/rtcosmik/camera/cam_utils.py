02096e6e8bb7549533535f8cb97bb0e99bb50361
mathiste1706 <mathilde1706.lalanne@gmail.com>
2026-05-15 15:11:08 +0200

    deleted unused functions, finished the functions header

--- src/rtcosmik/camera/cam_utils.py
+++ src/rtcosmik/camera/cam_utils.py
@@ -8,7 +8,7 @@ def list_cameras():
     """
     Use v4l2-ctl to list all connected cameras and their device paths.
     Return:
-    cameras: dictionary of camera indices and associated device names.
+        cameras: dictionary of camera indices and associated device names.
     """
     cameras = {}
     try:
@@ -31,10 +31,10 @@ def rt_to_homogeneous(R, translation_matrix):
     """
     Convert (R, translation_matrix) to a 4x4 homogeneous transformation matrix.
     Parameters: 
-    R is a rotation matrix (3x3)
-    translation_matrix is a translation matrix (3x1)
-    Return:
-    T: homogenous translation matrix (4x4)
+        R (numpy.ndarray): rotation matrix (3x3)
+        translation_matrix (numpy.ndarray): translation matrix (3x1)
+    Returns:
+        T (numpy.ndarray): homogeneous translation matrix (4x4)
     """
     
     translation_matrix = translation_matrix.reshape(3,)
@@ -47,9 +47,9 @@ def invert_homogeneous(T):
     """
     Invert a 4x4 homogeneous transformation matrix.
     Parameter: 
-    T: homogenous translation matrix (4x4)
-    Return:
-    T_inv: the inverse matrix of T
+        T (numpy.ndarray): homogeneous translation matrix (4x4)
+    Returns:
+        T_inv (numpy.ndarray): the inverse matrix of T
     """
     
     R = T[:3, :3]
@@ -63,273 +63,132 @@ def decompose_homogeneous(T):
     """
     Extract (R, translation_matrix) from T a 4x4 homogeneous matrix.
     Parameter:
-    T: a 4x4 homogeneous matrix
+        T (numpy.ndarray): a 4x4 homogeneous matrix
     Returns:
-    R: Rotation matrix (3x3) from transformation matrix T
-    translation_matrix: translation_matrix (3x1) from transformation matrix T
+        R (numpy.ndarray): Rotation matrix (3x3) from transformation matrix T
+        translation_matrix (numpy.ndarray): translation_matrix (3x1) from transformation matrix T
     """
     
     R = T[:3, :3]
     translation_matrix = T[:3, 3]
     return R, translation_matrix
 
-def get_cameras_params(K1, D1, K2, D2, R, translation_matrix2 translation_matrix1=[0.0, 0.0, 0.0]):
-   
-    """
-    dict_camera = {
-        "camera1": {
-            "matrix":np.array(K1),
-            "distortion_coeff":D1,
-            "rotation":np.eye(3),
-            "translation":[
-                0.,
-                0.,
-                0.,
-            ],
-        },
-        "camera2": {
-            "matrix":np.array(K2),
-            "distortion_coeff":D2,
-            "rotation":R,
-            "translation":translation_matrix2,
-        },
-    }
-
-    rotations_list=[]
-    translations_list=[]
-    distortion_coeff_list=[]
-    matrix_list=[]
-    projection_list=[]
-
-    for camera in dict_camera :
-        rotation=np.array(dict_camera[camera]["rotation"])
-        rotation_list.append(rotation)
-        translation=np.array([dict_camera[camera]["translation"]]).reshape(3,1)
-        translation_list.append(translation)
-        projection = np.concatenate([rotation, translation], axis=-1)
-        projection_list.append(projection)
-        dict_camera[camera]["projection"] = projection
-        distortion_coeff_list.append(dict_camera[camera]["distortion_coeff"])
-        matrix_list.append(dict_camera[camera]["matrix"])
-        """
-    
-    return projection_coeff_list, distortion_coeff_list, projection_list, rotation_list, translation_list
-
-def get_four_cameras_params(K1, D1, K2, D2, K3, D3, K4, D4, R2, T2, R3, T3, R4, T4):
-    dict_cam = {
-        "cam1": {
-            "matrix":np.array(K1),
-            "dist":D1,
-            "rotation":np.eye(3),
-            "translation":[
-                0.,
-                0.,
-                0.,
-            ],
-        },
-        "cam2": {
-            "matrix":np.array(K2),
-            "dist":D2,
-            "rotation":R2,
-            "translation":T2,
-        },
-        "cam3": {
-            "matrix":np.array(K3),
-            "dist":D3,
-            "rotation":R3,
-            "translation":T3,
-        },
-        "cam4": {
-            "matrix":np.array(K4),
-            "dist":D4,
-            "rotation":R4,
-            "translation":T4,
-        }
-    }
-
-    rotations=[]
-    translations=[]
-    dists=[]
-    mtxs=[]
-    projections=[]
-
-    for cam in dict_cam :
-        print(cam)
-        print(dict_cam[cam]["translation"])
-        
-        rotation=np.array(dict_cam[cam]["rotation"])
-        rotations.append(rotation)
-        translation=np.array([dict_cam[cam]["translation"]]).reshape(3,1)
-        translations.append(translation)
-        projection = np.concatenate([rotation, translation], axis=-1)
-        projections.append(projection)
-        dict_cam[cam]["projection"] = projection
-        dists.append(dict_cam[cam]["dist"])
-        mtxs.append(dict_cam[cam]["mtx"])
-    return mtxs, dists, projections, rotations, translations
-
 
 def load_cam_params(path):
     """
     Loads camera parameters from a given file.
-    Args:
+    Parameter:
         path (str): The path to the file containing the camera parameters.
     Returns:
         tuple: A tuple containing the camera matrix and distortion matrix.
             - camera_matrix (numpy.ndarray): The camera matrix.
-            - dist_matrix (numpy.ndarray): The distortion matrix.
+            - distortion_matrix (numpy.ndarray): The distortion matrix.
     """
     
     # FILE_STORAGE_READ
     cv_file = cv.FileStorage(path, cv.FILE_STORAGE_READ)
 
-    # note we also have to specify the type to retrieve other wise we only get a
+    # note we also have to specify the type to retrieve otherwise we only get a
     # FileNode object back instead of a matrix
     camera_matrix = cv_file.getNode('K').mat()
-    dist_matrix = cv_file.getNode('D').mat()
+    distortion_matrix = cv_file.getNode('D').mat()
 
     cv_file.release()
-    return camera_matrix, dist_matrix
+    return camera_matrix, distortion_matrix
 
 
 def load_cam_to_cam_params(path):
     """
     Loads camera-to-camera calibration parameters from a given file.
-    This function reads the rotation matrix (R) and translation vector (T) from a 
+    This function reads the rotation matrix R (3x3) and translation matrix (3x1) from a 
     specified file using OpenCV's FileStorage. The file should contain these parameters 
     stored under the keys 'R' and 'T'.
-    Args:
+    Parameter:
         path (str): The file path to the calibration parameters.
     Returns:
         tuple: A tuple containing:
             - R (numpy.ndarray): The rotation matrix.
-            - T (numpy.ndarray): The translation vector.
+            - translation_matrix (numpy.ndarray): The translation matrix.
     """
     
     # FILE_STORAGE_READ
     cv_file = cv.FileStorage(path, cv.FILE_STORAGE_READ)
 
-    # note we also have to specify the type to retrieve other wise we only get a
+    # note we also have to specify the type to retrieve otherwise we only get a
     # FileNode object back instead of a matrix
     R = cv_file.getNode('R').mat()
-    T = cv_file.getNode('T').mat()
+    translation_matrix = cv_file.getNode('T').mat()
 
     cv_file.release()
-    return R, T
+    return R, translation_matrix
 
 def load_global_cam_params(path, cam_index):
     """
     Loads the global camera transformation parameters for a specified camera
-    from a YAML file. This function reads the rotation matrix (R) and translation
-    vector (T) stored under the keys 'camera_{cam_index}_R' and 'camera_{cam_index}_T'.
+    from a YAML file. This function reads the rotation matrix R (3x3) and translation
+    matrix (3x1) stored under the keys 'camera_{cam_index}_R' and 'camera_{cam_index}_T'.
     
-    Args:
+    Parameters:
         path (str): The file path to the YAML file.
         cam_index (int): The camera index to load.
         
     Returns:
         tuple: A tuple containing:
             - R (numpy.ndarray): The rotation matrix.
-            - T (numpy.ndarray): The translation vector.
+            - translation_matrix (numpy.ndarray): The translation vector.
     """
     cv_file = cv.FileStorage(path, cv.FILE_STORAGE_READ)
     R = cv_file.getNode(f'camera_{cam_index}_R').mat()
-    T = cv_file.getNode(f'camera_{cam_index}_T').mat()
+    translation_matrix = cv_file.getNode(f'camera_{cam_index}_T').mat()
     cv_file.release()
-    return R, T
+    return R, translation_matrix
 
 
 def load_cam_pose(filename):
     """
-        Load the rotation matrix and translation vector from a YAML file.
-        Args:
+        Load the rotation matrix (3x3) and translation matrix (3x1) from a YAML file.
+        Parameters:
             filename (str): The path to the YAML file.
         Returns:
             rotation_matrix (np.ndarray): The 3x3 rotation matrix.
-            translation_vector (np.ndarray): The 3x1 translation vector.
+            translation_matrix (np.ndarray): The 3x1 translation matrix.
     """
 
     with open(filename, 'r') as file:
         data = yaml.safe_load(file)
 
     rotation_matrix = np.array(data['rotation_matrix']['data']).reshape((3, 3))
-    translation_vector = np.array(data['translation_vector']['data']).reshape((3, 1))
+    translation_matrix = np.array(data['translation_vector']['data']).reshape((3, 1))
     
-    return rotation_matrix, translation_vector
-
-def load_cam_pose_rpy(filename):
-    """
-        Load the euler angles and translation vector from a YAML file.
-        Args:
-            filename (str): The path to the YAML file.
-        Returns:
-            euler (np.ndarray): The 3x1 euler sequence.
-            translation_vector (np.ndarray): The 3x1 translation vector.
-    """
-
-    with open(filename, 'r') as file:
-        data = yaml.safe_load(file)
-
-    euler = np.array(data['rotation_rpy']['data']).reshape((3, 1))
-    translation_vector = np.array(data['translation_vector']['data']).reshape((3, 1))
-    
-    return euler, translation_vector
+    return rotation_matrix, translation_matrix
 
 
 def load_camera_parameters(config_path):
-    """Load intrinsic and extrinsic camera parameters."""
+    """
+    Load intrinsic and extrinsic camera parameters from a file.
+    Parameters:
+        config_path (str): The path to the configuration file.
+    Returns:
+        K1: the camera intrinsic matrix of camera 0.
+        D1: the camera distortion matrix of camera 0.
+        K2: the camera extrinsic matrix of camera 2.
+        D2: the camera distortion matrix of camera 2.
+        R: the rotation matrix between camera 0 and camera 2.
+        translation_matrix: the translation matrix between camera 0 and camera 2.
+    """
     K1, D1 = load_cam_params(os.path.join(config_path, "c0_params_color.yaml"))
     K2, D2 = load_cam_params(os.path.join(config_path, "c2_params_color.yaml"))
-    R, T = load_cam_to_cam_params(os.path.join(config_path, "c0_to_c2_params_color.yaml"))
-    return (K1, D1, K2, D2, R, T)
+    R, translation_matrix = load_cam_to_cam_params(os.path.join(config_path, "c0_to_c2_params_color.yaml"))
+    return K1, D1, K2, D2, R, translation_matrix
 
 def load_world_transformation(config_path):
-    """Load world transformation matrix."""
-    world_R1_cam, world_T1_cam = load_cam_pose(os.path.join(config_path, "camera0_pose.yaml"))
-    return world_R1_cam, world_T1_cam.reshape((3,))
-
-def load_intrinsic_cams(config_path):
-    """Load intrinsic and extrinsic camera parameters."""
-    K1, D1 = load_cam_params(os.path.join(config_path, "c0_params_color.yaml"))
-    K2, D2 = load_cam_params(os.path.join(config_path, "c2_params_color.yaml"))
-    K3, D3 = load_cam_params(os.path.join(config_path, "c4_params_color.yaml"))
-    K4, D4 = load_cam_params(os.path.join(config_path, "c6_params_color.yaml"))
-    return K1,D1,K2,D2,K3,D3,K4, D4
-
-def load_extrinsic_cams(config_path):
-    R02, T02 = load_cam_to_cam_params(os.path.join(config_path, "c0_to_c2_params_color.yaml"))
-    R24, T24 = load_cam_to_cam_params(os.path.join(config_path, "c2_to_c4_params_color.yaml"))
-    R46, T46 = load_cam_to_cam_params(os.path.join(config_path, "c4_to_c6_params_color.yaml"))
-    return R02, T02,R24, T24,R46, T46
-
-def compute_extrinsics_in_cam0(R02, T02, R24, T24, R46, T46):
     """
-    Returns extrinsics (R, T) of cams 0, 2, 4, 6 all expressed in cam0 frame.
+    Load world transformation matrix from a file.
+    Parameters:
+        config_path (str): The path to the configuration file.
+    Returns:
+        world_R1_cam (np.ndarray): The rotation matrix from camera 0 to the world.
+        world_translation1_cam (np.ndarray): The translation matrix from camera 0 to the world.
     """
-    # Build forward chain
-    T_0to2 = rt_to_homogeneous(R02, T02)
-    T_2to4 = rt_to_homogeneous(R24, T24)
-    T_4to6 = rt_to_homogeneous(R46, T46)
-
-    # Compute transforms to cam0 frame
-    T_0to4 = T_0to2 @ T_2to4
-    T_0to6 = T_0to4 @ T_4to6
-
-    # Decompose into (R, T)
-    R02, T02 = decompose_homogeneous(T_0to2)
-    R04, T04 = decompose_homogeneous(T_0to4)
-    R06, T06 = decompose_homogeneous(T_0to6)
-
-    return R02, T02,R04, T04,R06, T06
-
-def load_four_camera_parameters(config_path):
-    """Load intrinsic and extrinsic camera parameters."""
-    K1, D1 = load_cam_params(os.path.join(config_path, "c0_params_color.yaml"))
-    K2, D2 = load_cam_params(os.path.join(config_path, "c2_params_color.yaml"))
-    K3, D3 = load_cam_params(os.path.join(config_path, "c4_params_color.yaml"))
-    K4, D4 = load_cam_params(os.path.join(config_path, "c6_params_color.yaml"))
-    R1, T1= load_cam_to_cam_params(os.path.join(config_path, "c0_to_c2_params_color.yaml"))
-    R2, T2 = load_cam_to_cam_params(os.path.join(config_path, "c0_to_c4_params_color.yaml"))
-    R3, T3 = load_cam_to_cam_params(os.path.join(config_path, "c0_to_c6_params_color.yaml"))
-
-    return (K1, D1, K2, D2,K3, D3, K4, D4, R1, T1,R2, T2,R3, T3)
+    world_R1_cam, world_translation1_cam = load_cam_pose(os.path.join(config_path, "camera0_pose.yaml"))
+    return world_R1_cam, world_translation1_cam.reshape((3,))
